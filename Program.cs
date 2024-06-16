40a7a27f1b2adb12dde7004506b41a42965214c4
Евгений Кучма <desferto87@gmail.com>
2024-06-16 20:34:38 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkNumThreePicture
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalNumberOfPicture = 52;
+            int numberOfPictureInARow = 3;
+            int numberOfFilledRows = totalNumberOfPicture / numberOfPictureInARow;
+            int PicturesOverARow = totalNumberOfPicture % numberOfPictureInARow;
+
+            Console.WriteLine($"Количество полных рядов картин равно " +
+                $"{numberOfFilledRows}, " +
+                $"количество картин вне полного ряда {PicturesOverARow}.");
+        }
+    }
+}
